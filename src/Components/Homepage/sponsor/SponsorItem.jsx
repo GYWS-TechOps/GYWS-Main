683627a9712c0279981969765aaae3f93410edd7
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-03-13 11:23:28 +0530

    Link redirect issue

--- src/Components/Homepage/sponsor/SponsorItem.jsx
+++ src/Components/Homepage/sponsor/SponsorItem.jsx
@@ -1,15 +1,15 @@
-
+import { Link } from "react-router-dom";
 
 
 export default function SponsorItem({ name, logoUrl, websiteLink }) {
   return (
-    <a href={websiteLink} target="_blank" rel="noreferrer" className="slider-card">
+    <Link to={websiteLink} className="slider-card">
       <div className="slider-card-image">
         <img src={logoUrl} alt="sponsorImage" />
       </div>
       <div className="slider-card-heading">
         <h3>{name}</h3>
       </div>
-    </a>
+    </Link>
   );
 }
