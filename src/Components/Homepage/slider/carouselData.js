577fce3279cc7e19d83168439c41fdf137b679d8
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-03-28 01:00:52 +0530

    link changes

--- src/Components/Homepage/slider/carouselData.js
+++ src/Components/Homepage/slider/carouselData.js
@@ -6,6 +6,12 @@ const carouselData = [
         alt: 'Aashayein',
     },
     {
+        // legend: "Do something today that your future self will thank you for",
+        legend: "IITians empowering youth for better tomorrow",
+        img: "/images/DSC_0046.JPG",
+        alt: 'Aashayein',
+    },
+    {
         legend: "IITians empowering youth for better tomorrow",
         img: "/images/header_bg_1.jpg",
         alt: 'Students at Jagriti Vidya Mandir(JVM)',
