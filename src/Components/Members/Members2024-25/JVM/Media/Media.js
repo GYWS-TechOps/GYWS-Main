a8cdbdbeeefdc9d3190b36bfb66c1db567e02c6d
Harry <harsagra3478@gmail.com>
2024-06-13 08:46:30 +0530

    added details for light team

--- src/Components/Members/Members2024-25/JVM/Media/Media.js
+++ src/Components/Members/Members2024-25/JVM/Media/Media.js
@@ -23,12 +23,12 @@ const Data = [
         email: "anmol.singh@gyws.org"
     },
     {
-        name: "Ananya Sikdar",
-        position: "Media Head",
-        imageUrl: "/images/profile.png",
-        facebookLink: "https://m.facebook.com/profile.php?id=100007010851430",
-        linkedinLink: "https://www.linkedin.com/in/anmol-singh-55025b259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-        email: "anmol.singh@gyws.org"
+        "name": "Ananya Sikdar",
+        "position": "Media Head",
+        "imageUrl": "/assets/images/members/Heads24/AnanyaSikdar.jpg",
+        "facebookLink": "https://www.facebook.com/profile.php?id=61560681846015&mibextid=JRoKGi",
+        "linkedinLink": "https://in.linkedin.com/comm/in/ananya-sikdar-82b20a26b?lipi=urn%3Ali%3Apage%3Aemail_email_notification_single_search_appearance_01%3BbvXvgqYETD2khDOmKg4LDQ%3D%3D&midToken=AQGvP01H2NSctg&midSig=0WGYCAVpGGQXg1&trk=eml-email_notification_single_search_appearance_01-header-0-profile_glimmer&trkEmail=eml-email_notification_single_search_appearance_01-header-0-profile_glimmer-null-ibv28z~lx35ws27~t6-null-null&eid=ibv28z-lx35ws27-t6&otpToken=MTMwNDFlZTgxMTJmY2ZjNWJkMmIwZmViNDExZmUwYjI4OGNjZDk0MzlhYWY4NzY4N2JjZjA1Njc0NjViNThmMWZmZDdkNGU5NTBkMmM3ODE1NDkyZDQ5YzQ4ZjI2NWE4YzE3NzljZDA2ZDc3MDNjNTc5YTAzNmE4LDEsMQ%3D%3D",
+        "email": "ananya.sikdar@gyws.org"
     },
 ]
 
