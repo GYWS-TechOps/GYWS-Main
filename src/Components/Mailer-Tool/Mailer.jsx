fcea62cc4a421b0b223cf0b4c0a5d548adcb911c
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-06-13 14:00:02 +0530

    bcc added

--- src/Components/Mailer-Tool/Mailer.jsx
+++ src/Components/Mailer-Tool/Mailer.jsx
@@ -36,6 +36,8 @@ function Sender() {
     const subject = document.getElementById("subject").value;
     const EMAIL = document.getElementById("email").value;
     const EMAIL_APP_PASSWORD = document.getElementById("apppassword").value;
+    const BCC = document.getElementById("bcc").value
+  
     const message = quill.root.innerHTML;
     if (!name || !subject || !EMAIL || !EMAIL_APP_PASSWORD || !message) {
       toast.error("Please fill in all fields");
@@ -70,12 +72,13 @@ function Sender() {
     formData.append("RECIEVER_MAILS", JSON.stringify(emails));
     formData.append("MAIL_SUBJECT", subject);
     formData.append("MAIL_BODY", message);
+    formData.append("BCC", BCC);
 
     toast.success(
       `Wait for ${emails.length * 15} Seconds, We are sending the mails..`
     );
     const response = await fetch(
-      "https://mailer-backend-h8rh.onrender.com/api//product/getbill",
+      "https://gyws-backend-ptg5.onrender.com/api/Mail",
       {
         method: "POST",
         body: formData,
@@ -99,7 +102,7 @@ function Sender() {
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
           aria-hidden="true"
         >
-          <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[orange] to-[orange] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem] extra"></div>
+          <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem] extra"></div>
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -226,6 +229,24 @@ function Sender() {
 
             <div className="sm:col-span-2">
               <label
+                htmlFor="bcc"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Bcc
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="bcc"
+                  id="bcc"
+                  autoComplete="organization"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
                 className="block text-sm font-semibold leading-6 text-gray-900"
                 htmlFor="file_input"
               >
@@ -246,7 +267,7 @@ function Sender() {
           <div className="mt-10">
             <button
               type="button"
-              className="block w-full rounded-md bg-orange-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               onClick={handlesend}
             >
               Send
