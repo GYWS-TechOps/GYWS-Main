8bd87cfe1b048406ff0ecc839ce7984ddae32197
Amlesh <amleshkr396@gmail.com>
2024-10-24 18:30:22 +0530

    removed the admins routes from main website

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -9,10 +9,10 @@ import HostelCons from "./Components/Donation/Hostel_Contruction.jsx";
 import HostelSus from "./Components/Donation/Hostel_Sustainability.jsx";
 import Donate from "./Pages/Donate/Donate.jsx";
 import Mailer from "./Components/Mailer-Tool/Mailer.jsx";
-import AdminPanel from "./Components/admin/adminPanel.jsx";
-import MembersForm from "./Components/admin/MembersForm.jsx";
-import EditMembersForm from "./Components/admin/EditMembersForm.jsx";
-import MembersAdmin from "./Components/admin/MemberForAdmin/MembersAdmin.jsx";
+// import AdminPanel from "./Components/admin/adminPanel.jsx";
+// import MembersForm from "./Components/admin/MembersForm.jsx";
+// import EditMembersForm from "./Components/admin/EditMembersForm.jsx";
+// import MembersAdmin from "./Components/admin/MemberForAdmin/MembersAdmin.jsx";
 import MemberPage from "./Components/Members/MembersAlpha/membersPage.jsx"; // Import MemberPage
 
 export default function NavRoutes() {
@@ -30,10 +30,10 @@ export default function NavRoutes() {
         <Route path="/donate/LiGHT" element={<LightDonate />} />
         <Route path="/Yoadmin/Mailer" element={<Mailer />} />
         <Route path="*" element={<h1 style={{ height: "100vh" }}>404: Page Not Found</h1>} />
-        <Route path="/secret/adminpanel" element={<AdminPanel />} />
+        {/* <Route path="/secret/adminpanel" element={<AdminPanel />} />
         <Route path="/secret/membersadmin" element={<MembersAdmin />} />
         <Route path="/secret/membersform" element={<MembersForm />} />
-        <Route path="/secret/editmembersform" element={<EditMembersForm />} />
+        <Route path="/secret/editmembersform" element={<EditMembersForm />} /> */}
         <Route path="/member/*" element={<MemberPage year={2024} />} />
       </Routes>
     </>
